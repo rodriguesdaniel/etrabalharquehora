1d05b166b3055583ac1bff6b850f438c6350f1ce
Daniel Rodrigues <daniel.ydeia@gmail.com>
2021-05-22 17:01:42 -0300

    Preload images function

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,12 +9,36 @@ let messages  = [
   ['Certo', 'cj-06.gif'],
 ];
 
-function randomContent () {
+// Create a array to get all images for preload function.
+let imageList = [];
+for (var imageTotal = 0; imageTotal < messages.length; imageTotal++) {
+  imageList.push(messages[imageTotal][1]);
+}
+
+/**
+ * Preload all images to fast loading page.
+ * @param {imageList}
+ */
+function preLoadImages() {
+  let images = [];
+  for (var i = 0; i < preLoadImages.arguments.length; i++) {
+    images[i] = [];
+    images[i].src = preLoadImages.arguments[i];
+  }
+}
+
+/**
+ * Show a random message and background when the page goes loaded or refreshed.
+ * @return {image} The image for background.
+ * @return {message} Random message for html.
+ */
+function randomContent() {
   let i = Math.floor(messages.length * Math.random());
   document.getElementById("message").innerHTML = '<h1 class="message">' + messages[i][0] + '</h1>';
 
-  let imagem = messages[i][1];
-  document.body.style.backgroundImage = "url(" + folder + imagem +")";
+  let image = messages[i][1];
+  document.body.style.backgroundImage = "url(" + folder + image +")";
 }
 
+preLoadImages(imageList);
 randomContent();
